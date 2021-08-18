e616bf95114313862fa294e5cd405a8f938e0f6b
Amrita D Singh <amdesingh92@gmail.com>
2021-08-17 22:07:27 -0700

    LogIn Modal Open / Close
    The JavaScript code for opening and closing the LogIn Modal

--- huluMain.js
+++ huluMain.js
@@ -1,1 +1,30 @@
-// JavaScript Document
+// JavaScript Document Name: huluMain.js
+
+// DOM Elements
+const modal = document.querySelector('.modal'); //Bring in the LogIn Modal
+const loginBtn = document.querySelector('.login-btn');
+const closeBtn = document.querySelector('.close');
+
+// Event Listeners
+loginBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Functions
+function openModal()
+{
+	modal.style.display = 'block'; // Show the LogIn Modal
+}
+
+function closeModal()
+{
+	modal.style.display = 'none'; // Hide the LogIn Modal
+}
+
+function outsideClick(e) // Close the LogIn Modal by clicking anywhere
+{
+	if(e.target == modal)
+	{
+		closeModal();		
+	}
+}
